7b5c32e2d06763a0226ca3ad2f7fa018f369fb3c
tiann <923551233@qq.com>
2015-12-26 20:53:33 +0800

    solve p14

---
+++ haskell99/p14/main.hs
@@ -0,0 +1,15 @@
+--(*) Duplicate the elements of a list.
+--
+--Example:
+--
+-- * (dupli '(a b c c d))
+--(A A B B C C C C D D)
+--Example in Haskell:
+--
+-- > dupli [1, 2, 3]
+--[1,1,2,2,3,3]
+
+dupli :: [a] -> [a]
+dupli = foldr copy []
+  where
+    copy x acc = x: x: acc
